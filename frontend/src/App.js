40c03f7477aa535af30f8e7fc76e3803014074fa
Suraj0327 <suraj.202074101@vcet.edu.in>
2023-08-02 11:47:11 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
-
-import {Route} from "react-router-dom";
-import Homepage from"./pages/homePage";
-import Chatpage from "./pages/chatPage";
-import './App.css';
-
+import Signin from "./Pages/Signin.js"
+import Signup from "./Pages/Signup.js"
+import Home from "./Pages/Home.js"
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
+import { useContext } from "react"
+import { AuthContext } from "./Context/AuthContext.js"
 
 function App() {
+  
+  const { user } = useContext(AuthContext)
+  
   return (
-  <div className="App">
-  <Route path="/" component={Homepage} exact/> 
-  <Route path="/chats" component={Chatpage}/>
-   </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            {user ? <Home /> : <Signin />}
+          </Route>
+          <Route exact path='/signin'>
+            {user ? <Redirect to='/' /> : <Signin />}
+          </Route>
+          <Route exact path='/signup'>
+            {user ? <Redirect to='/' /> : <Signup/>}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
